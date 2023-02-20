b517834b8f0ddefef503a86b57aa2fdf65c6b22c
John Henry Thompson <jht1493@gmail.com>
2023-02-20 02:08:45 -0500

    init

---
+++ AppStorageDemo/AppStorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageDemoApp.swift
+//  AppStorageDemo
+//
+//  Created by jht2 on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
